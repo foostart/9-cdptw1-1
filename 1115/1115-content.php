0dd90a967eb8892c899be11ae6b1ba5c9b0fcd26
jone-dema <ducle199903@gmail,com>
2019-09-28 14:41:56 +0700

    ketthuc

---
+++ 1115/1115-content.php
@@ -0,0 +1,64 @@
+<div class="people-slide">
+    <div class="container">
+        <div class="people-slide-text">
+            <div class="people-text active-slide" stt="0">
+                <i class="fa fa-quote-left"></i>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore Ventage or dorent eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident rum.
+                <h6>MICHAEL STROMBACH</h6>
+                <i class="fa fa-quote-right"></i>
+            </div>
+            <div class="people-text" stt="1">
+                <i class="fa fa-quote-left "></i>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident
+                <h6>STROMBACH MICHAEL</h6>
+                <i class="fa fa-quote-right "></i>
+            </div>
+        </div>
+
+        <div class="people-pic">
+            <i class="fa fa-chevron-left"></i>
+            <ul>
+                <li><img src="images/team-10-70x70.jpg"></li>
+                <li><img src="images/team-9-70x70.jpg"></li>
+            </ul>
+            <i class="fa fa-chevron-right"></i>
+        </div>
+    </div>
+</div>
+
+<!---------------LOGO SHOW---------------------->
+<div class="container logo-sliderow">
+    <!--            <i class="fa fa-chevron-left"></i>-->
+
+
+    <div class="autoplay" stt="1">
+        <!-- logo-slide -->
+        <div class="logo-item "><img src="images/logo-1-1.jpg"></div>
+        <div class="logo-item "><img src="images/logo-6-1.jpg"></div>
+        <div class="logo-item "><img src="images/logo-2.jpg"></div>
+        <div class="logo-item "><img src="images/logo-3-1.jpg"></div>
+        <div class="logo-item "><img src="images/logo-4-1.jpg"></div>
+        <div class="logo-item"><img src="images/logo-1-1.jpg"></div>
+        <div class="logo-item"><img src="images/logo-2-1.jpg"></div>
+        <div class="logo-item"><img src="images/logo-6-1.jpg"></div>
+        <div class="logo-item"><img src="images/logo-1-1.jpg"></div>
+        <div class="logo-item"><img src="images/logo-2.jpg"></div>
+
+    </div>
+    <!--            <i class="fa fa-chevron-right"></i>-->
+
+
+</div>
+<!---------------LOGO SHOW-------------------->
+<script type="text/javascript">
+
+    $('.autoplay').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+</script>
