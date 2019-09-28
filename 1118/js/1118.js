5a954e1d0c8dab0deb8195b865c0fd8b634d012e
jone-dema <ducle199903@gmail,com>
2019-09-28 14:43:05 +0700

    ketthuc

---
+++ 1118/js/1118.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.right= "0px";
+   
+  }
+
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.right = "-100%";
+    $(".my_overplay").hide();
+  }
---
+++ 1119/1119-content.php
@@ -0,0 +1,18 @@
+
+        <div class="type-1119">
+            <div class="container">
+                <div class="row">
+                    <div class="footer">
+                        <div class="pull-right">
+                            Gentelella - Bootstrap Admin Template by <a href="#">Colorlib</a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+ 
+
+
+
